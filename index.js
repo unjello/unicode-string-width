37d7b2c27a7d1fb0de141f751b5c6a42c2727ba3
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-05-13 09:17:04 +0200

    Work around iOS/OSX's extra variation selector
    According to http://www.iemoji.com/view/emoji/503/symbols/heavy-large-circle,
    for some characters, a special variation selector was added, that needs
    to be accounted for.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const ansiRegex = require('ansi-regex')()
 const isFullwidthCodePoint = require('is-fullwidth-code-point')
 
+const isVariationSelector = c => c >= 0xfe00 && c <= 0xfe0f
+
 module.exports = s => {
   if (!s || s.length === 0) return 0;
   
@@ -10,6 +12,12 @@ module.exports = s => {
   const graphemes = [...s]
   for (let i = 0; i < graphemes.length; i++) {
     const code = graphemes[i].codePointAt(0)
+
+    // according to: http://www.iemoji.com/view/emoji/503/symbols/heavy-large-circle
+    // some emojis that used to be 1 character, were added a variation selector to be
+    // easily identified and shown in OSX and iOS
+    if (isVariationSelector(code)) continue
+
     count += isFullwidthCodePoint(code) ? 2 : 1;
   }
 
