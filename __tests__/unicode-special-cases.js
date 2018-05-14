c985b6bc34ac2c061cf86fdb1a1dc52264877402
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-05-14 22:06:29 +0200

    Add emoji to Apple's iOS/macOS exception case

--- __tests__/unicode-special-cases.js
+++ __tests__/unicode-special-cases.js
@@ -2,7 +2,7 @@ const unicodeStringWidth = require('../index')
 
 describe('unicode special cases', () => {
   test('for extra variant selector in apple systems', () => {
-    ['⚠️', '🛑'].forEach(e => expect(unicodeStringWidth(e)).toEqual(1))
+    ['⚠️', '🛑', '🚷'].forEach(e => expect(unicodeStringWidth(e)).toEqual(1))
   })
 
   test('combining characters should be ignored', () => {
