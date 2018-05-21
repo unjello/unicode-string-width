ea777af3e2c92f84c01518af84464ef36300329d
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-05-21 22:20:12 +0200

    Make linter happy

--- __tests__/unicode-special-cases.js
+++ __tests__/unicode-special-cases.js
@@ -1,8 +1,10 @@
 const unicodeStringWidth = require('../index')
 
 describe('unicode special cases', () => {
-  test('for extra variant selector in apple systems', () => {
-    ['⚠️', '🛑', '🚷'].forEach(e => expect(unicodeStringWidth(e)).toEqual(1))
+  ['⚠️', '🛑', '🚷'].forEach(e => {
+    test(`for extra variant selector in apple '${e}' (${[...e].map((c, i) => e.codePointAt(i))})`, () => {
+      expect(unicodeStringWidth(e)).toEqual(2)
+    })
   })
 
   test('combining characters should be ignored', () => {
--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const isAsciiControlCharacter = c => c <= 0x1f || (c >= 0x7f && c <= 0x9f)
  * Calculates a width in characters of a unicode string. Most unicode chars don't match monospace
  * anyway, so don't be surprised if it won't look pretty in the console. Counts full-width characters
  * and emoji as 2.
- * 
+ *
  * Supports emojis with an extra Variation Selector, [introduced](http://www.iemoji.com/view/emoji/503/symbols/heavy-large-circle)
  * by iOS/macOS.
- * 
+ *
  * @param {string} s - unicode string
  * @returns {number} - width in characters
  */
@@ -34,16 +34,16 @@ module.exports = s => {
       (code >= 0x1F600 && code <= 0x1F64F) || // Emoticons
       (code >= 0x1F300 && code <= 0x1F5FF) || // Misc Symbols and Pictographs
       (code >= 0x1F680 && code <= 0x1F6FF) || // Transport and Map
-      (code >= 0x2600 && code <= 0x26FF) ||   // Misc symbols
-      (code >= 0x2700 && code <= 0x27BF) ||   // Dingbats
-      (code >= 0xFE00 && code <= 0xFE0F) ||   // Variation Selectors
+      (code >= 0x2600 && code <= 0x26FF) || // Misc symbols
+      (code >= 0x2700 && code <= 0x27BF) || // Dingbats
+      (code >= 0xFE00 && code <= 0xFE0F) || // Variation Selectors
       (code >= 0x1F900 && code <= 0x1F9FF) || // Supplemental Symbols and Pictographs
-      (code >= 65024 && code <= 65039) ||     // Variation selector
-      (code >= 8400 && code <= 8447)          // Combining Diacritical Marks for Symbols
+      (code >= 65024 && code <= 65039) || // Variation selector
+      (code >= 8400 && code <= 8447) // Combining Diacritical Marks for Symbols
     ) {
-        if (code >= 0x1F3FB && code <= 0x1F3FF) continue; // emoji modifier fitzpatrick type
-        count += 2;
-        continue;
+      if (code >= 0x1F3FB && code <= 0x1F3FF) continue // emoji modifier fitzpatrick type
+      count += 2
+      continue
     }
 
     if (isCombiningCharacter(code) || isAsciiControlCharacter(code)) continue
